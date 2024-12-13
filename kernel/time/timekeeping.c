1fff02e2346163de6382bbf53f302ecd6b67086e
Rissu J <rissu.ntk@gmail.com>
2024-12-13 05:50:24 +0000

    kernel: Import OEM Changes
    Signed-off-by: Rissu J <rissu.ntk@gmail.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -1689,6 +1689,9 @@ void timekeeping_resume(void)
 	u64 cycle_now, nsec;
 	bool inject_sleeptime = false;
 
+	if (!timekeeping_suspended)
+		return;
+
 	read_persistent_clock64(&ts_new);
 
 	clockevents_resume();
