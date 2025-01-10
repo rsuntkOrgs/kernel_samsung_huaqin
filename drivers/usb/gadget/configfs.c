848404c31fe150ef488c5a135c3c8973265401dc
Rissu J <rissu.ntk@gmail.com>
2025-01-10 09:19:43 +0000

    Revert "ANDROID: usb: gadget: configfs: Move CONFIGFS_UEVENT #endif"
    This reverts commit 5568061d603d940d611c3878a3e2e6711afca121.
    
    Signed-off-by: Rissu J <rissu.ntk@gmail.com>

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -1586,30 +1586,7 @@ static void configfs_composite_disconnect(struct usb_gadget *gadget)
 	composite_disconnect(gadget);
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
-
-static void configfs_composite_reset(struct usb_gadget *gadget)
-{
-	struct usb_composite_dev *cdev;
-	struct gadget_info *gi;
-	unsigned long flags;
-
-	cdev = get_gadget_data(gadget);
-	if (!cdev)
-		return;
-
-	gi = container_of(cdev, struct gadget_info, cdev);
-	spin_lock_irqsave(&gi->spinlock, flags);
-	cdev = get_gadget_data(gadget);
-	if (!cdev || gi->unbind) {
-		spin_unlock_irqrestore(&gi->spinlock, flags);
-		return;
-	}
-
-	composite_reset(gadget);
-	spin_unlock_irqrestore(&gi->spinlock, flags);
-}
-
-#endif // CONFIG_USB_CONFIGFS_UEVENT
+#endif
 
 static void configfs_composite_unbind(struct usb_gadget *gadget)
 {
